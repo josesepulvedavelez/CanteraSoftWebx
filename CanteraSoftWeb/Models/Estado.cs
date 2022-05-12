e85530fda1b44e90acc99ef834eac4dbe6d7a2ee
Jose <jose.sepulveda@linktic.co>
2022-05-12 12:57:40 -0500

    Eliminar cliente
    En realidad no lo elimina, lo cambia de estado.
    
    Al cargar el datagrid principal selecciona todos diferentes del estado 2 (Eliminado)

---
+++ CanteraSoftWeb/Models/Estado.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CanteraSoftWeb.Models
+{
+    public class Estado
+    {
+        public Estado()
+        {
+        }
+
+        public int Id { get; set; }
+        public string _Estado { get; set; }
+    }
+}
