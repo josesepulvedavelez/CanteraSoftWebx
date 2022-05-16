6b814bc69d0d59e2b762dbd3998fa6fefbe613f0
Jose <jose.sepulveda@linktic.co>
2022-05-16 18:14:55 -0500

    Vehiculo Index y Vehiculo actualizar
    Operaciones de listar los vehiculos y actualizar vehiculo uno a uno.

---
+++ CanteraSoftWeb/Dto/VehiculoDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace CanteraSoftWeb.Dto
+{
+    public class VehiculoDto
+    {
+        public VehiculoDto()
+        {
+        }
+
+        public int VehiculoId { get; set; }
+        public string Placa { get; set; }
+        public double Mt3 { get; set; }
+        public int ClienteId { get; set; }
+        public string Nombre { get; set; }
+        public int Estado { get; set; }
+        public string Observaciones { get; set; }
+        public DateTime FechaLog { get; set; }
+        public string UsuarioLog { get; set; }
+    }
+}
---
+++ CanteraSoftWeb/Models/VehiculoModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace CanteraSoftWeb.Models
+{
+    public class VehiculoModel
+    {
+        public VehiculoModel()
+        {
+        }
+
+        public int VehiculoId { get; set; }
+        public string Placa { get; set; }
+        public double Mt3 { get; set; }
+        public int ClienteId { get; set; }
+        public int Estado { get; set; }
+        public string Observaciones { get; set; }
+        public DateTime FechaLog { get; set; }
+        public string UsuarioLog { get; set; }
+    }
+}
